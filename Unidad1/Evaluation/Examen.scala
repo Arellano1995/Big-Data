55e43582747385711f4e2762702c96d8a9b52d2b
danielarenas <flavio.castro@tectijuana.edu.mx>
2020-02-23 10:48:54 -0800

    Examen

--- Unidad1/Evaluation/Examen.scala
+++ Unidad1/Evaluation/Examen.scala
@@ -1,4 +1,9 @@
+def examen() = {
 var arreglo0 = Array(10,5,20,20,4,5,2,25,1) 
 val y =arreglo0(0)+arreglo0(4)+arreglo0(8)
 val x =arreglo0(2)+arreglo0(4)+arreglo0(6)
-x-y
+
+val sum = x+y
+math.abs(sum)
+}
+examen 
