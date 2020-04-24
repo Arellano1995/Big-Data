264dc29b161727d5b8edf03f232e6abc8dd52256
danielarenas <flavio.castro@tectijuana.edu.mx>
2020-04-24 09:38:00 -0700

    practice 1 dan

--- Practicas/AssigmentLinearRegression.scala
+++ Practicas/AssigmentLinearRegression.scala
@@ -4,21 +4,22 @@
 /////////////////////////////////////////
 
 // Import LinearRegression
-
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.sql.SparkSession
 // Opcional: Utilice el siguiente codigo para configurar errores
 import org.apache.log4j._
 Logger.getLogger("org").setLevel(Level.ERROR)
 
 
 // Inicie una simple Sesion Spark
-
+val spark = SparkSession.builder().getOrCreate()
 // Utilice Spark para el archivo csv Clean-Ecommerce .
-
+val data  = spark.read.option("header","true").option("inferSchema", "true").format("csv").load("Clean-Ecommerce.csv")
 // Imprima el schema en el DataFrame.
-
+data.printSchema()
 
 // Imprima un renglon de ejemplo del DataFrane.
-
+data.head(1)
 
 
 //////////////////////////////////////////////////////
@@ -27,15 +28,30 @@ Logger.getLogger("org").setLevel(Level.ERROR)
 
 // Transforme el data frame para que tome la forma de
 // ("label","features")
+val colnames = data.columns
+val firstrow = data.head(1)(0)
+println("\n")
+println("Example data row")
+for(ind <- Range(1, colnames.length)){
+    println(colnames(ind))
+    println(firstrow(ind))
+    println("\n")
+    }
 
+val logregdataall = (data.select(data("Email").as("label"), $"Avatar", $"Avg Session Length",
+                    $"Time on App", $"Time on Website", $"Length of Membership", $"Yearly Amount Spent"))
+
+val logregdata = logregdataall.na.drop()
 // Importe VectorAssembler y Vectors
-
+import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer, VectorIndexer, OneHotEncoder}
+import org.apache.spark.ml.linalg.Vectors
 // Renombre la columna Yearly Amount Spent como "label"
 // Tambien de los datos tome solo la columa numerica 
 // Deje todo esto como un nuevo DataFrame que se llame df
 
 // Que el objeto assembler convierta los valores de entrada a un vector
-
+val genderIndexer = new StringIndexer().setInputCol("Sex").setOutputCol("SexIndex")
+val embarkIndexer = new StringIndexer().setInputCol("Embarked").setOutputCol("EmbarkIndex")
 
 // Utilice el objeto VectorAssembler para convertir la columnas de entradas del df
 // a una sola columna de salida de un arreglo llamado  "features"
@@ -56,8 +72,18 @@ Logger.getLogger("org").setLevel(Level.ERROR)
 // Resuma el modelo sobre el conjunto de entrenamiento imprima la salida de algunas metricas!
 // Utilize metodo .summary de nuestro  modelo para crear un objeto
 // llamado trainingSummary
+val Array(training, test) = logregdata.randomSplit(Array(0.7, 0.3), seed = 12345)
 
+import org.apache.spark.ml.Pipeline
+
+val lr = new LogisticRegression()
+
+// val pipeline = new Pipeline().setStages(Array(genderIndexer,embarkIndexer,embarkEncoder,assembler,lr))
+val pipeline = new Pipeline().setStages(Array(genderIndexer,embarkIndexer,genderEncoder,embarkEncoder,assembler,lr))
+
+val model = pipeline.fit(training)
 // Muestre los valores de residuals, el RMSE, el MSE, y tambien el R^2 .
-
+println("Confusion matrix:")
+println(metrics.confusionMatrix)
 
 // Buen trabajo!
